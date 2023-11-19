51acaed70ae52605bb01f6c5d8ba53a3e0368b86
Igor <you@example.com>
2023-11-20 01:04:00 +0200

    add create_all in main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from routers.users_router import router as user_router
+
+from users_router import router as user_router
+from database import Base, engine
 
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/v1")
+
+Base.metadata.create_all(bind=engine)
