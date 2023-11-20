ae869ee34473a384e4fd86752c92d3a098f38711
Igor <you@example.com>
2023-11-20 23:01:11 +0200

    refactor:  remove DAL in users_router

--- routers/users_router.py
+++ routers/users_router.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 
 from database import get_db
-import schemas, repositories
+import schemas, models
 
 
 router = APIRouter()
@@ -10,13 +10,18 @@ router = APIRouter()
 
 @router.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = repositories.get_users(db, skip=skip, limit=limit)
+    users = db.query(models.User).offset(skip).limit(limit).all()
     return users
 
 
 @router.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = repositories.get_user_by_email(db, email=user.email)
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return repositories.create_new_user(db=db, user=user)
+    fake_hashed_password = user.password + "TO_DO"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
