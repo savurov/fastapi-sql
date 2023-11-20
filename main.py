ca0a9f05c57095a4b7c65e07b7e448716fc36158
Igor <you@example.com>
2023-11-20 23:02:53 +0200

    refactor: Remove create_all as no need

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
-from users_router import router as user_router
-from database import Base, engine
+from routers.users_router import router as user_router
+from routers.messages_router import router as message_router
+
 
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/v1")
+app.include_router(message_router, prefix="/v1")
 
-Base.metadata.create_all(bind=engine)
+# from database import Base, engine
+# Base.metadata.create_all(bind=engine)
